c8ceae42f66562c76a955488f6a36dc25a078603
alicemingle <65029188+alicemingle@users.noreply.github.com>
2020-10-15 14:28:00 +0300

    Update CIntNR.h

--- CIntNR.h
+++ CIntNR.h
@@ -1,16 +1,20 @@
-#pragma once
 #include <iostream>
 #define LENGTH 5
 
 using namespace std;
-class CIntNR {
+class CIntNR
+{
 private:
-    int vect[LENGTH];
-    int mod;
+	int mod;
+	int vect[LENGTH];
+	friend void autotest1();
+	friend void autotest2();
 public:
-    CIntNR(); // Конструктор по умолчанию
-    CIntNR(int N, int* coord_arr); // Конструктор, N - параметр кольца вычетов, coord_arr- массив, первое число - колво коорд.
-    CIntNR operator+(const CIntNR& arg);
-    CIntNR operator-(const CIntNR& arg);
-    void print();
+	CIntNR(); // РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
+	CIntNR(int N);
+	CIntNR(int N, int* coord_arr); // РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ, N - РїР°СЂР°РјРµС‚СЂ РєРѕР»СЊС†Р° РІС‹С‡РµС‚РѕРІ, coord_arr- РјР°СЃСЃРёРІ, РїРµСЂРІРѕРµ С‡РёСЃР»Рѕ - РєРѕР»РІРѕ РєРѕРѕСЂРґ.
+	CIntNR operator+(const CIntNR& arg);
+	CIntNR operator-(const CIntNR& arg);
+	void print();
 };
+
