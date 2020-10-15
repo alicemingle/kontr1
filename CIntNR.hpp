645a90ca96693d9950c85464710e6874bf13c62c
alicemingle <65029188+alicemingle@users.noreply.github.com>
2020-10-15 14:32:22 +0300

    Add files via upload

---
+++ CIntNR.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define LENGTH 5
+
+using namespace std;
+class CIntNR
+{
+private:
+	int mod;
+	int vect[LENGTH];
+	friend void autotest1();
+	friend void autotest2();
+public:
+	CIntNR(); // Конструктор по умолчанию
+	CIntNR(int N);
+	CIntNR(int N, int* coord_arr); // Конструктор, N - параметр кольца вычетов, coord_arr- массив, первое число - колво коорд.
+	CIntNR operator+(const CIntNR& arg);
+	CIntNR operator-(const CIntNR& arg);
+	void print();
+};
